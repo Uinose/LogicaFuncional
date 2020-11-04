b9fef085772a999df9840dce9bf356dce3feace8
Marisol Realivazquez <66392967+Uinose@users.noreply.github.com>
2020-11-04 00:41:07 -0700

    Primeras funciones en Haskell

---
+++ funcional/introduccion/funciones.hs
@@ -0,0 +1,10 @@
+--Cómo hacer una suma
+--Public void suma(a,b) (return a+b;)
+--ghc -o ejecutable ./funciones.hs crear ejecutable
+--ghci entrar al prelude
+suma a b= a + b
+resta a b= a - b 
+sumar a b c d e= a + b + c + d + e
+--Usar comillas francesas
+division a b= a `div` b 
+pregunta a= not a 
