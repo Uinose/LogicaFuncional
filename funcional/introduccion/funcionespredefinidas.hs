2d58bd014f383cd28c5d7be96e2ec85b311c55eb
Marisol Realivazquez <66392967+Uinose@users.noreply.github.com>
2020-11-24 22:45:04 -0700

    Practica funciones predefinidas

---
+++ funcional/introduccion/funcionespredefinidas.hs
@@ -0,0 +1,15 @@
+funcion1 x= abs x
+funcion2 x= ceiling x
+funcion3 x= succ x
+funcion4 x y= min x y
+funcion5 x y= max x y 
+funcion6 x y= x `div` y
+funcion7 x y= x `mod` y
+funcion8 x= even x    --verifica si es par
+funcion9 x= floor x
+funcion10 x y= gcd x y
+funcion11 x y= not (x+y /= x)
+funcion12 x y= not (x+y == x)
+funcion13 x= odd x
+funcion14 x= signum x
+funcion15 x y= snd (x,y)
